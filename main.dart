c4dc90ff64b8559b489db8fb94c960de72d31c20
Caio Vinicius <caioviniciuscv18@gmail.com>
2022-05-22 01:13:07 -0300

    learning how to convert map to list

--- main.dart
+++ main.dart
@@ -1,3 +1,21 @@
 void main(List<String> args) {
   print("jesus alive!");
+  var myMap = {
+    'myName': "Caio Vinicius",
+    'myAge': 22,
+    'myParents': [
+      "Marta Pereira Cesar da Silva",
+      "Luiz Antonio Bezerra da Silva"
+    ],
+  };
+  print(myMap);
+  var myArray = [];
+  myMap.forEach((key, value) {
+    myArray.add(value);
+  });
+  var myParents = [];
+  myParents = myArray[2];
+  for (var element in myParents) {
+    print(element);
+  }
 }
