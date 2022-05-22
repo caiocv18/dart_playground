477d470c608484d471c7128dda053d21fca198df
Caio Vinicius <caioviniciuscv18@gmail.com>
2022-05-22 03:03:51 -0300

    using int, double and boolean to practice

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
-void main(List<String> args) {
-  print("jesus alive!");
+void main(List<String> args) {}
+
+void learningMapToList() {
   var myMap = {
     'myName': "Caio Vinicius",
     'myAge': 22,
@@ -17,5 +18,34 @@ void main(List<String> args) {
   myParents = myArray[2];
   for (var element in myParents) {
     print(element);
+  }
+}
+
+void intAndDouble() {
+//int idade = 22;
+  int age = 0x000016;
+  double height = 1.70; //using hex
+  double largeHeight = 170e6;
+  print(age);
+  print(height);
+  print(largeHeight);
+}
+
+void usingBoolean() {
+  bool male = true;
+  int age = 22;
+  bool canDrive = age > 16;
+  print("Is he male?");
+  if (male) {
+    print("Yes");
+  } else {
+    print("no");
+  }
+  print("He can't drive, right?");
+  if (!canDrive) {
+    print("Yes");
+  }
+  {
+    print("No");
   }
 }
