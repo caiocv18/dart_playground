d9fff9b278d336244ccea8982e4c9a8bb83ac82e
Caio Vinicius <caioviniciuscv18@gmail.com>
2022-05-26 02:52:03 -0300

    Class 3 - List, const, final and var (DONE)

---
+++ Classes/class3.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+class Class3 {
+  void listExercise() {
+    List names = [];
+    String? contineButton = "";
+    do {
+      print("Add a name for your team:");
+      names.add(stdin.readLineSync());
+      print("Continue? (Y/N)");
+      contineButton = stdin.readLineSync();
+    } while (contineButton?.toUpperCase() == "Y");
+    print(Process.runSync("cls", [], runInShell: true).stdout);
+    for (var element in names) {
+      print((names.indexOf(element) + 1).toString() + " - " + element);
+    }
+  }
+
+// arrowListExercise() => () {
+//       List<String> names = [];
+//       String contineButton = "";
+//       do {
+//         print("Add a name for your team:");
+//         names.add(stdin.readLineSync()!);
+//         print("Continue? (Y/N)");
+//         contineButton = stdin.readLineSync()!;
+//       } while (contineButton.toUpperCase() == "Y");
+//       print(Process.runSync("cls", [], runInShell: true).stdout);
+//       for (var element in names) {
+//         print(element);
+//       }
+//     };
+
+  void dynamicList() {
+    List<dynamic> person = ["Caio Vinicus", 22, 1.70, true];
+    print(person.toString());
+  }
+
+  void finalAndConst() {
+    const name = "Caio";
+    final otherName = "Vinicius";
+    const String adress = "Residencial Esplanada";
+    final String adress2 = "Taguatinga Norte";
+    var anything = "Qualquer coisa";
+  }
+}
--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
-import 'Classes/class2.dart';
+import 'Classes/class3.dart';
 
 void main(List<String> args) {
-  Class2().exerciseClass2();
+  //Class2().exerciseClass2();
+  Class3().listExercise();
+  //dynamicList();
 }
