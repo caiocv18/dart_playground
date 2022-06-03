c93b204adf50ec464c3e4ae9cbcd7116273df6b5
Caio Vinicius <caioviniciuscv18@gmail.com>
2022-06-03 07:46:35 -0300

    Adding every, where, take, skip and expand methods

--- Classes/class4.dart
+++ Classes/class4.dart
@@ -58,5 +58,31 @@ class Class4 {
         .map((e) => e % 2 == 0 ? e : 0)
         .reduce((value, element) => value + element);
     print(sumOfPairs);
+    List<Map> myMap = [
+      {"Name": "Caio Vinicius", "Age": 22, "Job": "Systems Analyst"},
+      {"Name": "Iara Alves", "Age": 21, "Job": "Student"}
+    ];
+    print("===========EVERY===========");
+    var isAdult = myMap.every((element) => element["Age"] > 18);
+    print(isAdult);
+    print("===========WHERE===========");
+    var onlyIara = myMap.where((element) => element["Name"] == "Iara Alves");
+    var onlyAdults = myMap.where((element) => element["Age"] > 18);
+    var onlyOlders = myMap.where((element) => element["Age"] > 60);
+    print(onlyIara);
+    print(onlyAdults);
+    print(onlyOlders);
+    print("============TAKE===========");
+    print(numbers.take(3));
+    print("============SKIP===========");
+    print(numbers.skip(5));
+    print("=========LIST.FROM=========");
+    List newNumbers = List.from(numbers);
+    print(newNumbers);
+    print("===========EXPAND==========");
+    List list = [1, 2, 3];
+    var expandedList =
+        list.expand((element) => [element, element * 2, element * 3]);
+    print(expandedList);
   }
 }
